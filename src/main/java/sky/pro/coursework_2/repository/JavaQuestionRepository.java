2073dc1d427f0ec7abda1379a8f64ecdd194eeb3
Юрий <fedejkin@gmail.com>
2024-08-23 13:18:32 +0300

    course-2 with corrections

--- src/main/java/sky/pro/coursework_2/repository/JavaQuestionRepository.java
+++ src/main/java/sky/pro/coursework_2/repository/JavaQuestionRepository.java
@@ -1,5 +1,6 @@
 package sky.pro.coursework_2.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import sky.pro.coursework_2.model.Question;
 
@@ -9,19 +10,19 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@Service("javaRepository")
+@Repository("javaRepository")
 public class JavaQuestionRepository implements QuestionRepository {
 
     private final Set<Question> questions = new HashSet<>();
 
-    @PostConstruct
-    void init() {
-        add("java_question_2", "java_answer_2");
-        add("java_question_3", "java_answer_3");
-        add("java_question_1", "java_answer_1");
-        add("java_question_4", "java_answer_4");
-        add("java_question_5", "java_answer_5");
-    }
+//    @PostConstruct
+//    void init() {
+//        add("java_question_2", "java_answer_2");
+//        add("java_question_3", "java_answer_3");
+//        add("java_question_1", "java_answer_1");
+//        add("java_question_4", "java_answer_4");
+//        add("java_question_5", "java_answer_5");
+//    }
 
     @Override
     public Question add(String question, String answer) {
--- src/main/java/sky/pro/coursework_2/repository/MathQuestionRepository.java
+++ src/main/java/sky/pro/coursework_2/repository/MathQuestionRepository.java
@@ -1,5 +1,6 @@
 package sky.pro.coursework_2.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import sky.pro.coursework_2.model.Question;
 
@@ -9,18 +10,18 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@Service("mathRepository")
+@Repository("mathRepository")
 public class MathQuestionRepository implements QuestionRepository {
     private final Set<Question> questions = new HashSet<>();
 
-    @PostConstruct
-    void init() {
-        add("math_question_1", "math_answer_1");
-        add("math_question_2", "math_answer_2");
-        add("math_question_3", "math_answer_3");
-        add("math_question_4", "math_answer_4");
-        add("math_question_5", "math_answer_5");
-    }
+//    @PostConstruct
+//    void init() {
+//        add("math_question_1", "math_answer_1");
+//        add("math_question_2", "math_answer_2");
+//        add("math_question_3", "math_answer_3");
+//        add("math_question_4", "math_answer_4");
+//        add("math_question_5", "math_answer_5");
+//    }
 
     @Override
     public Question add(String question, String answer) {
